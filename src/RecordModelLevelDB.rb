aaa52eecfdda81199e75e560f80faf455070bb65
Michael Neumann <mneumann@ntecs.de>
2011-12-02 22:42:18 +0100

    Block version for #open

--- src/RecordModelLevelDB.rb
+++ src/RecordModelLevelDB.rb
@@ -1,1 +1,19 @@
 require 'RecordModelLevelDBExt'
+
+class RecordModelLevelDB
+  class << self
+    alias _open open
+    def open(db, modelklass, &block)
+      if block
+        begin
+          db = _open(db, modelklass)
+          block.call(db)
+        ensure
+          db.close
+        end
+      else
+        _open(db, modelklass)
+      end
+    end 
+  end
+end
